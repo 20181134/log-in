bfe4ffa35d64cf1560b990c64c8d14cd519d2871
20181134 <91303066+20181134@users.noreply.github.com>
2021-11-11 11:05:05 +0900

    login system

---
+++ login.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Log-in System</title>
+    </head>
+    <body>
+        <h1>Log-in System</h1>
+        <form action="output.php" method="post">
+            Username: <input type="text" name="name">
+            Password: <input type="password" name="password">
+            <input type="submit" value="Log In">
+        </form>
+    </body>
+</html>
---
+++ output.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Output</title>
+    </head>
+    <body>
+    <?php
+        $pdo = new PDO('mysql:host=localhost;dbname=login;charset=utf8', 'admin', 'password');
+        $stmt = $pdo->prepare('SELECT FROM login_database where username="'.$_REQUEST['name'].'"');
+        if ($stmt->execute()) {
+            if ($_REQUEST['password'] == $row['password']) {
+                $status = 1;
+                echo 'ログインが完了しました';
+            } else {
+                $status = 0;
+                echo 'ユーザー名またはパスワードが違います';
+            }
+        } else {
+            $status = 0;
+            echo 'SQLに接続できません<br>';
+            echo print_r($stmt->errorInfo());
+        }
+        ?>
+    </body>
+</html>
