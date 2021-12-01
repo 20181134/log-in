1f730c5e4bf3a701c14ab96d25463f3f3d02e242
20181134 <91303066+20181134@users.noreply.github.com>
2021-12-01 11:05:53 +0900

    Add files via upload

--- output.php
+++ output.php
@@ -8,14 +8,16 @@
         unset($_SESSION['user']);
         $pdo = new PDO ('mysql:host=localhost;dbname=login;charset=utf8', 'admin', 'password');
         $sql=$pdo->prepare('select * from login_database where username=? and password=?');
-        foreach ($sql as $row) {
-            $_SESSION['user']=[
-                'username'=>$row['username'],
-                'password'=>$row['password']
-            ];
+        if ($sql->execute([$_REQUEST['name'], $_REQUEST['password']])) {
+            foreach ($sql as $row) {
+                $_SESSION['user']=[
+                    'username'=>$row['username'],
+                    'password'=>$row['password']
+                ];
+            }
         }
         if (isset($_SESSION['user'])) {
-            echo 'ようこそ、', $_SESSION['username'], 'さん';
+            echo 'ようこそ、', $_SESSION['user']['username'], 'さん';
         } else {
             echo 'ユーザー名またはパスワードが違います';
         }
